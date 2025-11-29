e67ae24592fcc94025b6751e703b5316c5cad498
siddhant1232 <siddhantgupta797400@gmail.com>
2025-11-29 11:45:39 +0530

    pushing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Dock } from 'lucide-react';
-import MacOSDock from './components/ui/mac-os-dock';
+
 import Backgound from './pages/Backgound';
 
-import DockDemo from './components/DockDemo-02';
-import DemoOne from "@/components/Dockitem";
+
+
 const App = () => {
   return(
   <>
